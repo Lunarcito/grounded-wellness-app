f505b3f29e9804ddb379d17cc244c0c011f12cef
Carolina Calle <calle.carolina@gmail.com>
2026-08-14 17:04:48 +0200

    ci: separate unit and end-to-end test suites

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -9,5 +9,6 @@ export default defineConfig({
     globals: true,
     setupFiles: ["./src/test/setup.ts"],
     include: ["**/*.{test,spec}.{ts,tsx}"],
+    exclude: [...configDefaults.exclude, "e2e/**"],
   },
 });
